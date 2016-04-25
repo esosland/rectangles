926389f11a4b470226aaa6cf3c7ee9a9a3ecf80d
Anthony Gold and Elissa Sosland <elissa@>
2016-04-25 10:31:26 -0700

    adds more unit testing and parameters

--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -8,4 +8,28 @@ public class RectangleTest {
     Rectangle testRectangle = new Rectangle(2,4);
     assertEquals(true, testRectangle instanceof Rectangle);
   }
+
+  @Test
+  public void getLength_getsRectangleLength_2() {
+    Rectangle testRectangle = new Rectangle(2, 4);
+    assertEquals(2, testRectangle.getLength());
+  }
+
+  @Test
+  public void getWidth_getsRectangleWidth_4() {
+    Rectangle testRectangle = new Rectangle(2, 4);
+    assertEquals(4, testRectangle.getWidth());
+  }
+
+  @Test
+  public void isSquare_whenNotASquare_false() {
+    Rectangle testRectangle = new Rectangle(2, 4);
+    assertEquals(false, testRectangle.isSquare());
+  }
+
+  @Test
+  public void isSquare_allSidesEqual_true() {
+    Rectangle testRectangle = new Rectangle(2, 2);
+    assertEquals(true, testRectangle.isSquare());
+  }
 }
